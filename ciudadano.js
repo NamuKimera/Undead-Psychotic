b62b6d389102c384ada9eadda1ab7f42e571564b
NamuKimeraVT <namuquimera@gmail.com>
2025-09-23 21:56:47 -0300

    creamos la clase asesino y ciudadano que hereda de persona y persona que hereda de gameObject para evitar la repeticion de codigo y mejorar la organizacion del proyecto

--- ciudadano.js
+++ ciudadano.js
@@ -1,4 +1,4 @@
-class Asesino extends Persona {
+class Ciudadano extends Persona {
     constructor() {
         
     }
--- personas.js
+++ personas.js
@@ -1,4 +1,4 @@
-class Personas {
+class Persona extends GameObject {
     constructor() {
         
     }
