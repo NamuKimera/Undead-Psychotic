7769ae116a266b23291a57a9c9652807761fa9a2
NamuKimeraVT <namuquimera@gmail.com>
2025-09-24 18:24:54 -0300

    Creacion de un nuevo archivo boid.js y vector2D.js para implementar el comportamiento de "boids" en los ciudadanos.

--- personas.js
+++ personas.js
@@ -2,4 +2,12 @@ class Persona extends GameObject {
     constructor() {
         
     }
+
+    cambiarDeSpriteSegunVelocidad() {
+    if (this.calcularVelocidadLineal() > 0) {
+      this.cambiarSpriteAnimado("caminar");
+    } else {
+      this.cambiarSpriteAnimado("parado");
+    }
+  }
 }
