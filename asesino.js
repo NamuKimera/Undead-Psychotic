831afc911aa9412bd91319d4e4b0e3d21dfd9374
NamuKimeraVT <namuquimera@gmail.com>
2025-09-30 17:37:48 -0300

    Agregado WASD

--- asesino.js
+++ asesino.js
@@ -1,43 +1,60 @@
 class Asesino extends GameObject {
-    constructor(texture, x, y, juego) {
-        super(texture, x, y, juego);
-    }
-    
-    movimientoWASD() {
-        const keys = {
+    constructor(textureData, x, y, juego) {
+        super(textureData, x, y, juego);
+
+        // Propiedades de movimiento
+        this.speed = 5; // Velocidad de movimiento
+        this.keys = {
             w: false,
             a: false,
             s: false,
             d: false
         };
 
+        // Inicializa el sprite del asesino (usando el contenedor de GameObject)
+        this.cambiarAnimacion("caminarAbajo"); // O la animación inicial que desees
+        this.container.x = x; // Posición inicial
+        this.container.y = y;
+        this.container.scale.set(2); // Escala el contenedor
+
+        // Agrega los listeners de teclado
+        this.setupKeyboardListeners();
+
+        // Agrega el ticker para la actualización del movimiento
+        this.juego.pixiApp.ticker.add(this.updateMovement.bind(this)); // Usa bind para mantener el contexto
+    }
+
+    setupKeyboardListeners() {
         window.addEventListener('keydown', (e) => {
-            if (e.key in keys) {
-                keys[e.key] = true;
+            if (e.key in this.keys) {
+                this.keys[e.key] = true;
             }
         });
-
         window.addEventListener('keyup', (e) => {
-            if (e.key in keys) {
-                keys[e.key] = false;
+            if (e.key in this.keys) {
+                this.keys[e.key] = false;
             }
         });
+    }
 
-        app.ticker.add(() => {
-        const speed = 5; // Velocidad de movimiento
-
-        if (keys.w) {
-            player.y -= speed;
+    updateMovement() {
+        if (this.keys.w) {
+            this.container.y -= this.speed;
+            this.cambiarAnimacion("caminarArriba");
         }
-        if (keys.s) {
-            player.y += speed;
+        if (this.keys.s) {
+            this.container.y += this.speed;
+            this.cambiarAnimacion("caminarAbajo");
         }
-        if (keys.a) {
-            player.x -= speed;
+        if (this.keys.a) {
+            this.container.x -= this.speed;
+            this.cambiarAnimacion("caminarDerecha");
+            this.spritesAnimados.caminarDerecha.scale.x = 2;
         }
-        if (keys.d) {
-            player.x += speed;
+        if (this.keys.d) {
+            this.container.x += this.speed;
+            this.cambiarAnimacion("caminarDerecha");
+            this.spritesAnimados.caminarDerecha.scale.x = -2;
         }
-        });
     }
 }
